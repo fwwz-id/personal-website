f3fb51f755dbbfeb25a4a37ca2ce8634c01dc6ef
fwwz-id <fwwz.id@gmail.com>
2025-10-16 11:44:13 +0700

    chore: refine sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,25 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: `https://${process.env.VERCEL_PRODUCTION_URL || 'fwwz.id'}`,
+      url: `https://${process.env.VERCEL_PRODUCTION_URL || 'personal.fwwz.space'}`,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: `https://${process.env.VERCEL_PRODUCTION_URL || 'fwwz.id'}/blog`,
+      url: `https://${process.env.VERCEL_PRODUCTION_URL || 'personal.fwwz.space'}/blog`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.5,
     },
     {
-      url: `https://${process.env.VERCEL_PRODUCTION_URL || 'fwwz.id'}/blog/rag-vs-mcp`,
+      url: `https://${process.env.VERCEL_PRODUCTION_URL || 'personal.fwwz.space'}/blog/rag-vs-mcp`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `https://${process.env.VERCEL_PRODUCTION_URL || 'personal.fwwz.space'}/blog/tidak-perlu-bootcamp-untuk-jadi-programmer`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
