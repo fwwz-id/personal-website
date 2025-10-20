5d6303ba9b327e4853143ba728eaa264886dfafd
fwwz-id <fwwz.id@gmail.com>
2025-10-20 19:48:09 +0700

    chore: fix robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,11 +1,13 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: ['/', '/api/og/*', '/blog', '/blog/*'],
+      userAgent: "*",
+      allow: ["/", "/api/og/*", "/blog", "/blog/*"],
     },
-    sitemap: `https://${process.env.VERCEL_PRODUCTION_URL}/sitemap.xml`,
-  }
+    sitemap: `https://${
+      process.env.VERCEL_PRODUCTION_URL || "personal.fwwz.space"
+    }/sitemap.xml`,
+  };
 }
